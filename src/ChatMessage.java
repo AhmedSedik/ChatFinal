ed4800245ee1d92961335c49fe7b18a1813e0f5c
yasseen-salama <yasseen.salama@gmail.com>
2020-02-05 13:50:17 +0100

    PlayResponse

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -17,7 +17,7 @@ public class ChatMessage implements Serializable {
     // The different types of message sent by the Client
     // MESSAGE an ordinary message
     // LOGOUT to disconnect from the Server
-    static final int OnlineUsers = 0, MESSAGE = 1, LOGOUT = 2, REPSONE_PLAY_REQUEST = 4;
+    static final int OnlineUsers = 0, MESSAGE = 1, LOGOUT = 2, REPSONE_PLAY_REQUEST = 4, PLAY = 5;
 
     private int type;
 
